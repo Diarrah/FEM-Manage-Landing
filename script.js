dca92fd83e8120b057d60bbb81098a745b2b097c
Diarrah <diarrahcasiano@gmail.com>
2020-04-11 21:02:57 -0700

    Footer form validation

---
+++ script.js
@@ -0,0 +1,25 @@
+/* Form Validation */
+let form = document.querySelector('.form-control');
+let error = form.querySelector('small');
+let email = form.querySelector('input');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let emailVal = email.value.trim();
+
+    if (emailVal === '') {
+        form.classList.add('error')
+        error.innerText = `Email cannot be empty`
+    } else if (!validateEmail(emailVal)) {
+        form.classList.add('error')
+        error.innerText = `Please provide a valid email`
+   } else { 
+       form.className = 'form-control'
+   }
+});
+
+/* ------ RegEx copied from StackOoverflow ------ */
+function validateEmail(email) {
+    let valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return valid.test(String(email).toLowerCase());
+}
