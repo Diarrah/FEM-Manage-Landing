ee05d52b5cd1684b852357dc917cd3243f0799cb
Diarrah <diarrahcasiano@gmail.com>
2020-04-15 16:34:44 -0700

    Swiper JS

---
+++ src/testimonial-swiper.js
@@ -0,0 +1,10 @@
+let swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      centeredSlides: true,
+      clickable: true,
+    },
+  });
