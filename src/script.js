367db9294ff8ba82c63dcec9d67620eb28f4e213
Diarrah <diarrahcasiano@gmail.com>
2020-04-15 16:34:06 -0700

    Form validation JS moved inside this folder

---
+++ src/script.js
@@ -0,0 +1,26 @@
+/* Form Validation */
+let form = document.querySelector('.form-control'),
+    email = form.querySelector('input'),
+    error = form.querySelector('small');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let emailVal = email.value.trim();
+
+    if (emailVal === '') {
+        form.classList.add('error')
+        error.innerText = `Email cannot be empty`;
+    } else if (!validateEmail(emailVal)) {
+        form.classList.add('error')
+        email.classList.add('error')
+        error.innerText = `Please provide a valid email`;
+   } else { 
+       form.className = 'form-control';
+   }
+});
+
+/* ------ RegEx copied from StackOverflow ------ */
+function validateEmail(email) {
+    let valid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return valid.test(String(email).toLowerCase());
+}
