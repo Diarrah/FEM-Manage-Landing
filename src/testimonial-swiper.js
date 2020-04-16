412a55fa27ec282e115a7732b2e30125a224baf6
Diarrah <diarrahcasiano@gmail.com>
2020-04-16 00:29:11 -0700

    Breakpoints added for Swiper in mobile

--- src/testimonial-swiper.js
+++ src/testimonial-swiper.js
@@ -1,6 +1,16 @@
 let swiper = new Swiper('.swiper-container', {
-    slidesPerView: 'auto',
-    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    breakpoints: {
+      550: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      930: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    },
     loop: true,
     pagination: {
       el: '.swiper-pagination',
